1a806dddbd059f72a2d560cce0c78d83840d2211
alex <adthoms@uwaterloo.ca>
2022-07-12 16:56:16 -0700

    add PCD format type option

--- src/loop_closed_paths_to_poses.cpp
+++ src/loop_closed_paths_to_poses.cpp
@@ -20,7 +20,7 @@ DEFINE_string(output_path, "",
 DEFINE_validator(output_path, &beam::gflags::ValidateDirMustExist);
 DEFINE_string(
     output_type, "JSON",
-    "Type of path file to output. Default: JSON. Options: JSON, PLY, TXT");
+    "Type of path file to output. Default: JSON. Options: JSON, PLY, TXT, PCD");
 DEFINE_int32(format_type, 1,
              "Format type of output. Default: 1. Options: 1, 2, 3");
 DEFINE_bool(interpolate_corrections, true,
